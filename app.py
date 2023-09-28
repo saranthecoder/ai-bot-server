59d13e6539b3a24a47f02db89aabb179ba1ff52f
saranthecoder <115061220+saranthecoder@users.noreply.github.com>
2023-09-28 11:35:18 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from chat import get_response
+import nltk
+
+# Set NLTK data path explicitly
+nltk.data.path.append('C:\\Users\\saran\\AppData\\Roaming\\nltk_data')
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def hello_world():
+    return "Hello, World!"
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    try:
+        data = request.get_json()
+        if "message" in data:
+            text = data["message"]
+            response = get_response(text)
+            message = {"answer": response}
+            return jsonify(message)
+        else:
+            return jsonify({"error": "Missing 'message' in request JSON"}), 400
+    except Exception as e:
+    # Log the error for debugging
+        print(f"An error occurred: {str(e)}")
+    # Return an error response to the client
+        return jsonify({"error": "An internal server error occurred"}), 500
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Determine the virtual environment activation script based on the operating system
+if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin" ]]; then
+    # Windows
+    activate_script="venv/Scripts/activate"
+else
+    # Unix-like systems
+    activate_script="venv/bin/activate"
+fi
+
+# Create and activate the virtual environment
+python -m venv venv
+source "$activate_script"
+
+# Install required packages
+pip install Flask torch torchvision nltk
+
+
+# Copy 'punkt' dataset to the default NLTK data path
+python -c "import nltk, shutil; shutil.copytree('nltk_data', nltk.data.find('corpora'))"
+
+
+# Install required Python packages from requirements.txt
+pip install -r requirements.txt
